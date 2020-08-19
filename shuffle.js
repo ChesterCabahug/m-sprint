d17c724f21508980fed5364a01b583d068c97439
Chester <chesterdeveloper2000@gmail.com>
2020-08-19 17:25:59 +0800

    generated random equations and placing it randomily as well

---
+++ shuffle.js
@@ -0,0 +1,25 @@
+function shuffle(array) {
+    let currentIndex = array.length;
+    let temporaryValue;
+    let randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+  
+    return array;
+  }
+  
+  // Used like so
+  const arr = [2, 11, 37, 42];
+  shuffle(arr);
+  console.log(arr);
